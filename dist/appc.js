3a822e205fec6a7a9e34e5022b53e212d85e2c39
sappiah085 <sappiah5689@gmail.com>
2025-03-01 20:46:43 +0100

    Initial commit

--- dist/appc.js
+++ dist/appc.js
@@ -53,20 +53,33 @@ io.on("connection", (socket) => {
     // Handle file sending
     socket.on("sendFileChunk", (data) => {
         const { targetUser, fileData, chunkNumber, totalChunks, fileName, fileType, } = data;
-        io.to(targetUser.id).emit("receiveChunk", {
-            fileData,
-            chunkNumber,
-            totalChunks,
-            fileName,
-            fileType,
-        });
-    });
-    socket.on("transfer-complete", ({ targetUser }) => {
-        io.to(targetUser.id).emit("transfer-complete");
-    });
-    socket.on("reject-transfer", ({ id }) => {
-        const user = users.find((user) => user.id === socket.id);
-        io.to(id).emit("transfer-interrupted", { sender: user });
+        console.log(`Receiving chunk ${chunkNumber + 1} of ${totalChunks} for file ${fileName}`);
+        // Initialize the file data storage if it doesn't exist
+        if (!fileChunks[targetUser.id]) {
+            fileChunks[targetUser.id] = {
+                chunks: [],
+                fileName,
+                fileType,
+            };
+        }
+        // Append the chunk to the user's file data
+        fileChunks[targetUser.id].chunks.push(fileData);
+        // If this is the last chunk, reconstruct the file
+        if (chunkNumber === totalChunks - 1) {
+            const buffer = Buffer.concat(
+            //Unit8Array(chunk) for ensuring each chunk in byte format i.e binary format as 1 and 0
+            // Then Buffer.from() converts this byte data into a buffer which is designed to handle binary data (raw data) directly
+            fileChunks[targetUser.id].chunks.map((chunk) => Buffer.from(new Uint8Array(chunk))));
+            console.log(`File ${fileName} received fully.`);
+            // Emit the file data to the recipient
+            io.to(targetUser.id).emit("receiveFile", {
+                fileName,
+                fileType,
+                fileData: buffer,
+            });
+            // Cleanup after sending the file
+            delete fileChunks[targetUser.id];
+        }
     });
     // Handle user disconnect
     socket.on("disconnect", (reason) => {
